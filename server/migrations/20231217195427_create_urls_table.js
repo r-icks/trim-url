0ef37afd4fbceb31e85802d1ed602aad9ee27f45
r-icks <rishijain1712@gmail.com>
2023-12-19 07:57:18 +0530

    migrationupdate

--- server/migrations/20231217195427_create_urls_table.js
+++ server/migrations/20231217195427_create_urls_table.js
@@ -1,9 +1,11 @@
 export const up = async (knex) => {
     await knex.schema.createTable('urls', (table) => {
       table.increments('urlId').primary();
-      table.string('shortName').unique().notNullable();
+      table.string('shortName').notNullable();
       table.text('redirectUrl').notNullable();
       table.integer('collectionId').unsigned().references('collections.collectionId').onDelete('CASCADE');
+
+      table.unique(['shortName', 'collectionId']);
     });
   };
   
